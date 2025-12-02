0c9a5587e38d7d33d67d0c527a71c7db1dcb7175
Sakari Ailus <sakari.ailus@linux.intel.com>
2025-12-03 00:34:42 +0200

    media: ipu6: Rework watermark calculation
    Move watermark calculation to ipu6-isys-csi2.c and do the whole thing
    as streaming is about to start. There's no need to store all the
    information so that it is bound to video nodes.
    
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>

--- drivers/media/pci/intel/ipu6/ipu6-isys-queue.c
+++ drivers/media/pci/intel/ipu6/ipu6-isys-queue.c
@@ -584,8 +584,6 @@ static int start_streaming(struct vb2_queue *q, unsigned int count)
 	}
 
 	list_add(&aq->node, &stream->queues);
-	ipu6_isys_configure_stream_watermark(av, source_pad->entity);
-	ipu6_isys_update_stream_watermark(av, true);
 
 	ret = ipu6_isys_stream_start(av);
 	if (ret)
@@ -596,7 +594,6 @@ static int start_streaming(struct vb2_queue *q, unsigned int count)
 	return 0;
 
 out_stream_start:
-	ipu6_isys_update_stream_watermark(av, false);
 	list_del(&aq->node);
 
 out_fw_close:
@@ -619,8 +616,6 @@ static void stop_streaming(struct vb2_queue *q)
 	struct ipu6_isys_stream *stream = av->stream;
 
 	mutex_lock(&stream->mutex);
-
-	ipu6_isys_update_stream_watermark(av, false);
 
 	mutex_lock(&av->isys->stream_mutex);
 	ipu6_isys_video_set_streaming(av, 0);
