843868aea059d922528f77c613c17e7b731c3f2a
Sakari Ailus <sakari.ailus@linux.intel.com>
2025-12-19 00:02:20 +0200

    media: ipu6: Move firmware init/cleanup to RPM callbacks
    Move handling firmware initialisation and cleanup away from the rest of
    the driver to the runtime PM callbacks. This simplifies the driver,
    including removing the ref_count field in struct ipu6_isys.
    
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>

--- drivers/media/pci/intel/ipu6/ipu6-isys-queue.c
+++ drivers/media/pci/intel/ipu6/ipu6-isys-queue.c
@@ -572,18 +572,11 @@ static int start_streaming(struct vb2_queue *q, unsigned int count)
 		goto out_pipeline_stop;
 	}
 
-	ret = ipu6_isys_fw_open(av->isys);
+	ret = ipu6_isys_stream_start(av);
 	if (ret)
 		goto out_pipeline_stop;
 
-	ret = ipu6_isys_stream_start(av);
-	if (ret)
-		goto out_fw_close;
-
 	return 0;
-
-out_fw_close:
-	ipu6_isys_fw_close(av->isys);
 
 out_pipeline_stop:
 	ipu6_isys_stream_cleanup(av);
@@ -606,8 +599,6 @@ static void stop_streaming(struct vb2_queue *q)
 	ipu6_isys_stream_cleanup(av);
 
 	return_buffers(aq, VB2_BUF_STATE_ERROR);
-
-	ipu6_isys_fw_close(av->isys);
 }
 
 static unsigned int
