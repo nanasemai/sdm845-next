RISHI27-dot/linux

media: uapi: Add more media bus codes for generic raw formats
Add new generic raw media bus codes for serial buses at bit depths or 16,
20, 24 and 28.

Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>

include/uapi/linux/media-bus-format.h
@@ -190,5 +190,9 @@
 #define MEDIA_BUS_FMT_RAW_10			0xa002
 #define MEDIA_BUS_FMT_RAW_12			0xa003
 #define MEDIA_BUS_FMT_RAW_14			0xa004
+#define MEDIA_BUS_FMT_RAW_16			0xa005
+#define MEDIA_BUS_FMT_RAW_20			0xa006
+#define MEDIA_BUS_FMT_RAW_24			0xa007
+#define MEDIA_BUS_FMT_RAW_28			0xa008
 
 #endif /* __LINUX_MEDIA_BUS_FORMAT_H */

nanasemai/sdm845-next

include/uapi/linux/media-bus-format.h
@@ -190,10 +190,14 @@
 #define MEDIA_BUS_FMT_META_20			0x8006
 #define MEDIA_BUS_FMT_META_24			0x8007
 
-/* Generic (CFA independent) pixel data formats. Next is 0x9005. */
+/* Generic (CFA independent) pixel data formats. Next is 0x9009. */
 #define MEDIA_BUS_FMT_RAW_8			0x9001
 #define MEDIA_BUS_FMT_RAW_10			0x9002
 #define MEDIA_BUS_FMT_RAW_12			0x9003
 #define MEDIA_BUS_FMT_RAW_14			0x9004
+#define MEDIA_BUS_FMT_RAW_16			0x9005
+#define MEDIA_BUS_FMT_RAW_20			0x9006
+#define MEDIA_BUS_FMT_RAW_24			0x9007
+#define MEDIA_BUS_FMT_RAW_28			0x9008
 
 #endif /* __LINUX_MEDIA_BUS_FORMAT_H */